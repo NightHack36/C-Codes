995355e03916f55148e005e5c2a6fea3f6d6b53f
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-10-23 12:32:20 +0500

    Sorting array

---
+++ java codes/src/com/company/sortArray.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sortArray {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.print("n = ");
+        n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++){
+            System.out.print("arr["+i+"] = ");
+            arr[i] = in.nextInt();
+        }
+        System.out.println("Sorted: ");
+        for (int i = 0; i < n; i++){
+            if (arr[i]>arr[i+1]){
+                arr[i] = arr[i] + arr[i+1];
+                arr[i+1] = arr[i] - arr[i+1];
+                arr[i] = arr[i] - arr[i+1];
+            }
+            System.out.println(arr[i]+"; ");
+        }
+    }
+}
