10575628003d191295c713cf3737df5ad752e4d6
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-10-23 13:02:07 +0500

    Sorting array

--- java codes/src/com/company/sortArray.java
+++ java codes/src/com/company/sortArray.java
@@ -3,7 +3,6 @@ package com.company;
 import java.util.Scanner;
 
 public class sortArray {
-//the code will replace elements of pairs in case the next element is smaller than previous
     public static void main(String[] args) {
         int n;
         Scanner in = new Scanner(System.in);
@@ -16,12 +15,14 @@ public class sortArray {
         }
         System.out.println("Sorted: ");
         for (int i = 0; i < n; i++){
-            if (arr[i]>arr[i+1]){
-                arr[i] = arr[i] + arr[i+1];
-                arr[i+1] = arr[i] - arr[i+1];
-                arr[i] = arr[i] - arr[i+1];
+            for (int j = i+1; j < n; j++){
+                if (arr[i]>arr[j]){
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
             }
-            System.out.println(arr[i]+"; ");
+            System.out.print(arr[i]+"; ");
         }
     }
 }
